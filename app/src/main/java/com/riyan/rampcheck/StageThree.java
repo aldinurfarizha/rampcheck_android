04a3cd16257cb2e8c440830d23c0e8baa6a64c1f
Aldi Nurfarizha <55512360+aldinurfarizha@users.noreply.github.com>
2023-09-19 23:16:31 +0700

    hasil ibu

---
+++ app/src/main/java/com/riyan/rampcheck/StageThree.java
@@ -0,0 +1,14 @@
+package com.riyan.rampcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StageThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stage_three);
+    }
+}
