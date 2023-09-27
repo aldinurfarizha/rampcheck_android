9707a6ca6e32054e28ef05e0f444968360f6d14b
aldinurfarizha <aldinurfarizha@gmail.com>
2023-09-27 22:14:40 +0700

    detail rampcheck

---
+++ app/src/main/java/com/riyan/rampcheck/Check/SelectDriver.java
@@ -0,0 +1,16 @@
+package com.riyan.rampcheck.Check;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.riyan.rampcheck.R;
+
+public class SelectDriver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_driver);
+    }
+}
---
+++ app/src/main/java/com/riyan/rampcheck/Data.java
@@ -0,0 +1,13 @@
+package com.riyan.rampcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+}
